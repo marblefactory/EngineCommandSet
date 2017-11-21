7bdc39992be6bec51ff791076176729c6aec321b
Albie Baker-Smith <bakersmitha@gmail.com>
2017-11-21 15:28:46 +0000

    Working on mac

--- src/client/client.h
+++ src/client/client.h
@@ -1,35 +1,35 @@
-//#include <iostream>
-//#include <thread>
-//#include <boost/asio.hpp>
-//#include <system_error>
-//
-//using namespace std;
-//
-//class Client {
-//private:
-//    const size_t MAX_LINESIZE = 100;
-//    boost::asio::io_service io_service;
-//    boost::asio::ip::tcp::socket* socket;
-//    boost::asio::ip::tcp::resolver::iterator endpoint_itr;
-//    bool shouldReconnect;
-//    bool isConnected;
-//    std::thread* Tlisten = nullptr;
-//    std::thread* Tconnect = nullptr;
-//
-//public:
-//    Client(){};
-//    bool setEndpoint(string host, string port);
-//    void startClient() { reconnect(); }
-//    bool stopClient();
-//
-//private:
-//    void listen();
-//    void setupSocket();
-//    void destroySocket();
-//    void reconnect();
-//    bool connect();
-//    int parseHeader(std::string* message) throw(boost::system::system_error);
-//    void readMessage(int message_length, std::string* message) throw(boost::system::system_error);
-//    bool checkServerDisconnected(boost::system::error_code errc);
-//};
+#include <iostream>
+#include <thread>
+#include <boost/asio.hpp>
+#include <system_error>
+
+using namespace std;
+
+class Client {
+private:
+    const size_t MAX_LINESIZE = 100;
+    boost::asio::io_service io_service;
+    boost::asio::ip::tcp::socket* socket;
+    boost::asio::ip::tcp::resolver::iterator endpoint_itr;
+    bool shouldReconnect;
+    bool isConnected;
+    std::thread* Tlisten = nullptr;
+    std::thread* Tconnect = nullptr;
+
+public:
+    Client(){};
+    bool setEndpoint(string host, string port);
+    void startClient() { reconnect(); }
+    void stopClient();
+
+private:
+    void listen();
+    void setupSocket();
+    void destroySocket();
+    void reconnect();
+    bool connect();
+    int parseHeader(std::string* message) throw(boost::system::system_error);
+    void readMessage(int message_length, std::string* message) throw(boost::system::system_error);
+    bool checkServerDisconnected(boost::system::error_code errc);
+};
 
